a3b970d44f58589fef47a25927d897ec83c4173f
kobso1245 <kobso1245@gmail.com>
2014-10-31 17:55:23 +0200

    Update

--- Racket_Homework/zad2_dom1.scm
+++
@@ -1,7 +0,0 @@
-(define (circle? x y radius)
-  (if ( <= ( + (* x x) (* y y)) (* radius radius)) #t 
-                                                    #f))
-        
-(circle? 2 2 3)
-(circle? 3 3 5)
-(circle? 4 3 3)
--- Racket_Homework/zad3_dom1.scm
+++
@@ -1,4 +0,0 @@
-(define (area a b c)
-  (sqrt (* (- (+ b c) a) (-(+ a b) c) (- (+ a c) b) (/ 1 16) (+ a b c))))
-(area 3 4 5)
-(area 4 7 8) 
